783c5450b8c597780982e20dd627c5d5d0b04c5a
Kaysyam <kaysyam07@gmail.com>
2026-04-17 20:33:46 +0800

    Update for.py

--- for.py
+++ for.py
@@ -1,15 +1,15 @@
 #penggunaak for looping dalam bentuk kata
-#or i in range(1, 11):
-# print(f"{i}. dosen pancasakti semuanya baik hati")
+for i in range(1, 11):
+  print(f"{i}. dosen pancasakti semuanya baik hati")
 
 #for menggunakan angka
-#angka2 = [0,1,2,3,4]
-#print(angka2)
+angka2 = [0,1,2,3,4]
+print(angka2)
 
-#for i in angka2:
-  #  print(f"i sekarang-> {i}")
+for i in angka2:
+    print(f"i sekarang-> {i}")
 
-#print("ini adalah akhir dari program")
+print("ini adalah akhir dari program")
 
 #sekarang adalah penggunaan while
 angka = 10
