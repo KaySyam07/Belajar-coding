c664c3359d56114a93593c0a7e4aa4de37c6e7b6
Kaysyam <kaysyam07@gmail.com>
2026-04-17 20:30:36 +0800

    Add files via upload
    izin koreksinya senior, guru, dosen, dan teman2

---
+++ for.py
@@ -0,0 +1,20 @@
+#penggunaak for looping dalam bentuk kata
+#or i in range(1, 11):
+# print(f"{i}. dosen pancasakti semuanya baik hati")
+
+#for menggunakan angka
+#angka2 = [0,1,2,3,4]
+#print(angka2)
+
+#for i in angka2:
+  #  print(f"i sekarang-> {i}")
+
+#print("ini adalah akhir dari program")
+
+#sekarang adalah penggunaan while
+angka = 10
+while angka > 5:
+    angka += 1
+    print("syam kece")
+
+print("cukuuuup")
